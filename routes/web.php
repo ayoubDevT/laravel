1f90d49a50e7237836152356aa664f604d092d0b
ayoubDevT <ayoubyafouze@gmail.com>
2021-02-01 03:18:41 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/', 'welcome')->middleware(['auth','verified']); fdf
+Route::view('/', 'welcome')->middleware(['auth','verified']);
